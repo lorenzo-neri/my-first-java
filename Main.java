6398a1e3dba609b93655c070e5ebb2c84a2ab9ad
lorenzo-neri <lorenzo21neri@gmail.com>
2024-02-09 19:45:09 +0100

    add second line

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
+//OOP
+
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello Java!");
+
+        System.out.println("Second line :)");
+
     }
 }
