270456f593f6579ee2742b1868f71ee89728ccaa
lorenzo-neri <lorenzo21neri@gmail.com>
2024-02-12 12:11:57 +0100

    type of data

--- Main.java
+++ Main.java
@@ -12,8 +12,11 @@ public class Main {
         System.out.println("Second line :) \n"); // \n si utilizza per andare a capo
 
         /*
+         * VARIABILI
+         * 
          * in Java bisogna indicare il tipo di valore della variabile
          */
+
         int x; // dichiarazione
         double temperatura;
 
@@ -26,5 +29,39 @@ public class Main {
         System.out.println(nome);
         System.out.println(temperatura);
 
+        /*
+         * TIPI DI DATI
+         * 
+         * - boolean | true : false | 1bit
+         * - byte | -128 : 127 | 1byte
+         * - short | -32768 : 32767 | 2 bytes
+         * - int | -2 miliardi : 2 miliardi | 4 bytes
+         * - long | -9 quinitilioni : 9 quintilioni | 8 bytes
+         * 
+         * - float | numero con 6-7 cifre decimali 5.123456f | 4 bytes
+         * - double | n con 15 decimali 5.123456789123456 | 8 bytes
+         * 
+         * - char | singolo carattere/lettera/ASCII 'f' |2 bytes
+         * - String | sequenza di caratteri "ciao questo è il mio primo file java" |
+         * variabile
+         * 
+         */
+
+        boolean ilBoolean = true;
+        byte ilByte = 127;
+        short loShort = -32768;
+        int ilInt = 2_000_000_000;
+        long ilLong = 89_436_234_874_458L;
+
+        float ilFloat = 5.1234567f; // f per il float è fondamentale
+        double ilDouble = 5.233453543;
+
+        char ilChar = 'c'; // il singolo apice per il char
+        String laStringa = "ciao"; // le virgolette per String
+
+        // primitive e reference
+        // con le reference possiamo utilizzare un insieme di metodi
+        System.out.println(laStringa.toUpperCase());
     }
+
 }
