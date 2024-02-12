e5f77999763b9c01fa3ba30874546b98d22cf1db
lorenzo-neri <lorenzo21neri@gmail.com>
2024-02-12 11:49:53 +0100

    variables

--- Main.java
+++ Main.java
@@ -4,9 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.print("Tutto su ");
+        System.out.print("una riga\n");
+
         System.out.println("Hello Java!");
 
-        System.out.println("Second line :)");
+        System.out.println("Second line :) \n"); // \n si utilizza per andare a capo
+
+        /*
+         * in Java bisogna indicare il tipo di valore della variabile
+         */
+        int x; // dichiarazione
+        double temperatura;
+
+        x = 45; // assegnazione
+        temperatura = 34.23;
+
+        String nome = "Lorenzo"; // inizializzazione
+
+        System.out.println(x);
+        System.out.println(nome);
+        System.out.println(temperatura);
 
     }
 }
