7f3ca060de80e7d95d00a0a96625408c83ed5c46
lorenzo-neri <lorenzo21neri@gmail.com>
2024-02-18 11:21:39 +0100

    access modifiers: public, protected, private

---
+++ OOP/Main_16.java
@@ -0,0 +1,27 @@
+package OOP;
+
+/* 
+ * Modificatori di accesso 
+ * 
+ * i modificatori di accesso servono per aggiungere un livello di sicurezza aggiuntivo
+ * 
+ * (li ho usati inconsciamente già in questo "tutorial" che ho fatto da solo, quindi andrò solo a prendere appunti)
+ * 
+ *              class      package       sublacss       world
+ * public       si         si            si             si
+ * protected    si         si            si             no
+ * default      si         si            no             no        
+ * private      si         no            no             no   
+ *      
+ * con public diamo l'accesso ovunque
+ * con protected si può avere l'accesso anche in sottoclassi che non sono nello stesso pacchetto
+ * private esclusivamente per utilizzo all'interno di una classe 
+ */
+
+//import OOP.Objects.*; * = importo tutto da quella cartella
+
+public class Main_16 {
+    public static void main(String[] args) {
+
+    }
+}
